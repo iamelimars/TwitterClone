76f450ea83d5e2df9ee997fa55ab3780c5f2aedd
Eli MArshall <elimarshall@Elis-Mac-mini.local>
2017-03-28 12:47:07 -0400

    Removed storyboards

---
+++ Twitter Clone/HomeController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeController.swift
+//  Twitter Clone
+//
+//  Created by Eli MArshall on 3/28/17.
+//  Copyright © 2017 Eli MArshall. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green
+    }
+}
